5081ed5ee71b616cf0cb6c43204240e875227f65
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2025-05-21 22:29:18 -0300

    finalizando primeiro capitulo do curso

--- alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
+++ alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -1,10 +1,38 @@
-﻿using ByteBankIO;
+﻿using System.Text;
+using ByteBankIO;
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        Console.ReadLine();
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; // 1KB
+
+
+
+        // public override int Read(byte[] array, int offset, int count);
+        // fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+            EscreverBuffer(buffer, numeroDeBytesLidos);
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+
+        var utf8 = new UTF8Encoding();
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+
+        // foreach (var meuByte in buffer)
+        // {
+        //     Console.Write($"{meuByte} ");
+        // }
     }
 }
