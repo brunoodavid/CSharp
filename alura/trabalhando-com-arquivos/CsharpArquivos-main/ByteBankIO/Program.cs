70305d29c00c76d76148187232fc2ddd14701d4e
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2025-05-21 23:02:28 -0300

    episodio 2 do capitulo 2 concluido

--- alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
+++ alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/Program.cs
@@ -5,24 +5,29 @@ class Program
 {
     static void Main(string[] args)
     {
-
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
-        var buffer = new byte[1024]; // 1KB
-
-
-
-        // public override int Read(byte[] array, int offset, int count);
-        // fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-        while (numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-            EscreverBuffer(buffer, numeroDeBytesLidos);
-        }
-    }
 
+            var numeroDeBytesLidos = -1;
+
+
+            var buffer = new byte[1024]; // 1KB
+
+
+
+            // public override int Read(byte[] array, int offset, int count);
+            // fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+        }
+
+
+    }
     static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
 
