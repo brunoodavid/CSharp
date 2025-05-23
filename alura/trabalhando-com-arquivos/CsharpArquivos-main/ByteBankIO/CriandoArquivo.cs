ab9ccc4c3ec4c3bb5f115e1115f5f4c60d5f7cb8
Bruno Hoffmann <bruno_davidthome@hotmail.com>
2025-05-22 22:44:27 -0300

    aula 03 do capitulo 4 concluida

--- alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/CriandoArquivo.cs
+++ alura/trabalhando-com-arquivos/CsharpArquivos-main/ByteBankIO/CriandoArquivo.cs
@@ -28,4 +28,22 @@ partial class Program
             escritor.Write("456, 65465, 456.0, Pedro");
         }
     }
+
+    static void TestaEscrita()
+    {
+        var caminhoNovoArquivo = "teste.txt";
+
+        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDoArquivo))
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                escritor.WriteLine($"Linha {i}");
+                escritor.Flush();
+
+                Console.WriteLine($"Linha {i} foi escrita no arquivo, Tecle enter...");
+                Console.ReadLine();
+            }
+        }
+    }
 }
